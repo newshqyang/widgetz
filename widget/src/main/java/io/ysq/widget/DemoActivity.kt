936996c07acbbda2c2f93a854059f886e5c008f2
YANGSHOUQIANG\yangshouqiang <yangshouqiang@hd123.com>
2021-01-29 17:34:33 +0800

    ClearEditText Perfect Version

---
+++ widget/src/main/java/io/ysq/widget/DemoActivity.kt
@@ -0,0 +1,14 @@
+package io.ysq.widget
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DemoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.demo_activity)
+
+        ClearEditTextKt(this)
+
+    }
+}
